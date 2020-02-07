78b7d10ba2ac93fbd39af197dac8c108d76a53ea
likamatisz <60703499+likamatisz@users.noreply.github.com>
2020-02-07 12:08:28 +0300

    Rename

--- Next and previous num.py
+++ Next and previous num.py
@@ -1,5 +1,5 @@
 num = int(input())
 nextNum = num + 1
-previousNum = num - 1
+preNum = num - 1
 print('The next number for the number ', num, ' is ', nextNum, '.', sep='')
-print('The previous number for the number ', num, ' is ', previousNum, '.', sep='')
+print('The previous number for the number ', num, ' is ', preNum, '.', sep='')
